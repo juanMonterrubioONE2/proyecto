4d17bd866cb7d1da4c91678ab7536d988197dc01
juanMonterrubioONE2 <juan_mr-marvel@hotmail.com>
2025-07-18 05:18:19 -0600

    Update frmCliente.cs
    cambios en frmclientes

--- Pry_gestor_de_mantenimiento/frmCliente.cs
+++ Pry_gestor_de_mantenimiento/frmCliente.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +12,77 @@ namespace Pry_gestor_de_mantenimiento
 {
     public partial class frmCliente : Form
     {
+
+        private clsClientes cliente;
         public frmCliente()
         {
             InitializeComponent();
+            cliente = new clsClientes();
+            dgvClientes.DataSource = null;
+            dgvClientes.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
+            dgvClientes.DataSource = cliente.CargarDatagridview();
+            LbFecha.Text = cliente.Lblhora;
+            // Configurar combos
+            // Cargar categorías de equipos
+            cmbCategorias.DataSource = cliente.ObtenerCategorias();
+            cmbCategorias.DisplayMember = "vchnombre";
+            cmbCategorias.ValueMember = "intidcategoria";
+
+            // Cargar marcas de equipos
+            cmbMarcas.DataSource = cliente.ObtenerMarcas();
+            cmbMarcas.DisplayMember = "vchnombre";
+            cmbMarcas.ValueMember = "intidmarca";
+
+            // Cargar estados de órdenes
+            cmbEstado.DataSource = cliente.ObtenerEstados();
+            cmbEstado.DisplayMember = "vchnombre";
+            cmbEstado.ValueMember = "intidestado";
+            // Mostrar datos iniciales
+
+        }
+
+
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Datos del usuario
+                cliente.ObtenerRoles();
+                cliente.Nombre = txtNombre.Text;
+                cliente.Apaterno = txtApaterno.Text;
+                cliente.Amaterno = txtAmaterno.Text;
+                cliente.Correo = txtCorreo.Text;
+                cliente.Password = txtPassword.Text;
+                cliente.Telefono = txtTelefono.Text;
+                cliente.Direccion = txtDireccion.Text;
+
+
+                // Datos del equipo (si aplica)
+                cliente.Noserie = txtNoSerie.Text;
+                cliente.Idcategoria = Convert.ToInt32(cmbCategorias.SelectedValue);
+                cliente.Idmarca = Convert.ToInt32(cmbMarcas.SelectedValue);
+                cliente.Idestado = Convert.ToInt32(cmbEstado.SelectedValue);
+                cliente.Modelo = txtModelo.Text;
+
+                // Datos de la orden (si aplica)
+                cliente.Descripcion = txtDescipcion.Text;
+
+                string resultado = cliente.Guardar();
+                MessageBox.Show(resultado);
+                cliente.limpiarCampos(this);
+
+                
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al guardar: " + ex.Message);
+            }
+        }
+
+        private void dgvClientes_SelectionChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
